4891686617e0bb38a060360676bc0ae363355e63
Seonghyun Cho <seonghyuncho19@gmail.com>
2017-04-02 10:01:42 -0400

    Counts individual vowels
    Sometimes Y

--- python/vowelCount.py
+++ python/vowelCount.py
@@ -1,20 +1,30 @@
-wordStart = 0
-vowelNum = 0
-
+vowels = [0, 0, 0, 0, 0]
 def vowelCount():
-    word = input("Insert any word or sentence for a vowel count!")
-    if(len(word) > 0 and word.isalpha()):
-        wordLength = len(word)
-        while(wordStart < wordLength):
-            if(word[wordStart] == "aeiou"):
-                vowelNum = vowelNum + 1
-                wordStart = wordStart + 1
-                print(str(vowelNum))
-            else:
-                wordStart = wordStart + 1
-                print(str(vowelNum))
+	word = input("Insert any word, phrase, or sentence for a vowel count. ")
+	#Check to see if they have letters
+	if(len(word) > 0):
+		wordLength = len(word)
+		#Checking each vowel
+		i = 0
+		while(i < wordLength):
+			if(word[i] == "a"):
+				vowels[0] = vowels[0] + 1
+				i = i + 1
+			elif(word[i] == "e"):
+				vowels[1] = vowels[0] + 1
+				i = i + 1
+			elif(word[i] == "i"):
+				vowels[2] = vowels[0] + 1
+				i = i + 1
+			elif(word[i] == "o"):
+				vowels[3] = vowels[0] + 1
+				i = i + 1
+			elif(word[i] == "u"):
+				vowels[4] = vowels[0] + 1
+				i = i + 1
+			else:
+				i = i + 1
 
-
-
+	print("There are ", vowels[0], " a's, ", vowels[1], " e's, ", vowels[2], " i's, ", vowels[3], " o's, ", vowels[4], " u's.")
+    
 vowelCount()
-
